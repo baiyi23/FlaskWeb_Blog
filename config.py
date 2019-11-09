a13a3be77cd078dcc139cc752815d69f6f82a29d
baiyi23 <45531653+baiyi23@users.noreply.github.com>
2019-11-09 19:45:44 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-class Config(object):
-    #设置密匙要没有规律，别被人轻易猜到哦
-    CSRF_ENABLED = True
-    SECRET_KEY = 'a9087FFJFF9nnvc2@#$%FSD'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR,'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
